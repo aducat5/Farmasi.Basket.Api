9d97540db4d666a5cfc64d865d5160d6980352ee
Tamer Berat Çelik <aducat5@hotmail.com>
2022-08-15 06:48:59 +0300

    Add project files.

---
+++ Farmasi.Basket.Api/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Farmasi.Basket.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+
+    }
+}
