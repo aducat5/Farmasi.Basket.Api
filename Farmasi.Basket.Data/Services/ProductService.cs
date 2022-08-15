e32902b21df0330e4b6c6708507fe7f90fdac1e0
Tamer Berat Çelik <aducat5@hotmail.com>
2022-08-15 07:02:41 +0300

    added data connection layer and init service

---
+++ Farmasi.Basket.Data/Services/ProductService.cs
@@ -0,0 +1,21 @@
+﻿using Farmasi.Basket.Model;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Farmasi.Basket.Data.Services
+{
+    public class ProductService
+    {
+        private readonly IMongoCollection<Product> _products;
+
+        public ProductService(IDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+            _products = database.GetCollection<Product>(settings.CollectionName);
+        }
+
+    }
+}
