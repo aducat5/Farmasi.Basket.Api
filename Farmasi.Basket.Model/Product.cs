ad4b6c4b08f6b9954cdd34071cdb632028470192
Tamer Berat Çelik <aducat5@hotmail.com>
2022-08-15 06:53:51 +0300

    added model project and product model

---
+++ Farmasi.Basket.Model/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Farmasi.Basket.Model
+{
+    public record Product
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Name { get; set; } = "";
+        public decimal Price { get; set; }
+        public bool IsStocked { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
