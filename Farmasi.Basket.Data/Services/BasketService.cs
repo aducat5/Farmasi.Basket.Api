b253474a90b57d9880b180425e1439d7486368ef
Tamer Berat Çelik <aducat5@hotmail.com>
2022-08-15 07:14:11 +0300

    added basket service init

---
+++ Farmasi.Basket.Data/Services/BasketService.cs
@@ -0,0 +1,16 @@
+﻿using Farmasi.Basket.Model;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Farmasi.Basket.Data.Services
+{
+    public class BasketService
+    {
+        public BasketService()
+        {
+
+        }
+    }
+}
