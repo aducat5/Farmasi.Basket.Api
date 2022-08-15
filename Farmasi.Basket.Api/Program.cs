7dcfb825fb903257fc65f781954b5577f2716f7a
Tamer Berat Çelik <aducat5@hotmail.com>
2022-08-15 07:33:29 +0300

    added services and low cors policy

--- Farmasi.Basket.Api/Program.cs
+++ Farmasi.Basket.Api/Program.cs
@@ -1,4 +1,5 @@
 using Farmasi.Basket.Data;
+using Farmasi.Basket.Data.Services;
 using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,7 +13,21 @@ builder.Services.Configure<DatabaseSettings>(
 builder.Services.AddSingleton<IDatabaseSettings>(sp =>
     sp.GetRequiredService<IOptions<DatabaseSettings>>().Value);
 
+//adding product services
+builder.Services.AddScoped<ProductService>();
+builder.Services.AddScoped<BasketService>();
+
+//adding controllers
 builder.Services.AddControllers();
+
+//enabling cors for client app
+builder.Services.AddCors(o => o.AddPolicy("LowCorsPolicy", builder => {
+    builder.AllowAnyOrigin()
+       .AllowAnyMethod()
+       .AllowAnyHeader();
+}));
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -26,6 +41,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+//use the new cors policy
+app.UseCors("LowCorsPolicy");
 
 app.UseHttpsRedirection();
 
